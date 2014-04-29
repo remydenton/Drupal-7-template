e7a861adb5d0ea44baf439dc9eca0eeb36d6edd0
Remy Denton <remy.denton@gmail.com>
2014-04-28 20:17:26 -0700

    Shorten $variables to $vars in template.php

--- sites/all/themes/base/template.php
+++ sites/all/themes/base/template.php
@@ -3,11 +3,11 @@
 /**
  * Implements hook_preprocess_page().
  */
-function base_preprocess_page(&$variables) {
+function base_preprocess_page(&$vars) {
 
   // Remove the titles from the main and secondary menus.
-  $variables['page']['main_menu']['#heading'] = t('');
-  $variables['page']['secondary_menu']['#heading'] = t('');
+  $vars['page']['main_menu']['#heading'] = t('');
+  $vars['page']['secondary_menu']['#heading'] = t('');
   
 }
 
